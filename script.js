d3647cd686e5ef10a837c0758dd24f60d3abc066
Klebertt <126410502+Klebertt@users.noreply.github.com>
2023-06-13 14:02:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById('botao').addEventListener('click', getData);
+
+function getData() {
+
+    fetch('https://randomuser.me/api/?results=1')
+        .then(res => res.json())
+        .then(data => {
+
+
+            let author = data.results;
+            let output = "<h2><center>Get User Data</center></h2>";
+
+            author.forEach(function (lists) {
+                output += `
+                <div class="container">
+                    <div class="card mt-4 bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h2>Nome: ${lists.name.first}</h2></li>
+                            <li id="foto" class="list-group-item"><img src="${lists.picture.large}"></li>
+                            <li class="list-group-item">Phone Number: ${lists.cell}</li>
+                            <li class="list-group-item">DOB: ${lists.dob.date}</li>
+                            <li class="list-group-item">Age: ${lists.dob.age}</li>
+                            <li class="list-group-item">Email ID: ${lists.email}</li>
+                            <li class="list-group-item">Gender: ${lists.gender}</li>
+                            <li class="list-group-item">City: ${lists.location.city}</li>
+                            <li class="list-group-item">Country: ${lists.location.country}</li>
+                            <li class="list-group-item">PostCode: ${lists.location.postcode}</li>
+                        </ul>
+                    </div>
+                </div> `;
+            });
+
+            document.getElementById('output').innerHTML = output;
+
+        });
+};
